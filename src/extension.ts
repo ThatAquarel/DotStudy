9cc99c0a3bd52e8bce2b98703eea3a6ba2c9c4e6
Aquarel <54329158+ThatAquarel@users.noreply.github.com>
2021-10-02 14:46:19 -0400

    Remove .gitignore files

--- src/extension.ts
+++ src/extension.ts
@@ -15,7 +15,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('test.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
+		// Display a mesgsage box to the user
 		vscode.window.showInformationMessage('Hello World from test!');
 	});
 
