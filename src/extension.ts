35e22bbd664058ba119bf91a78ed9b2a1a2ea795
Aquarel <54329158+ThatAquarel@users.noreply.github.com>
2021-10-03 13:07:23 -0400

    Raname Grammar

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,5 @@
 import * as vscode from 'vscode';
 
-export function activate(context: vscode.ExtensionContext) {}
+export function activate(context: vscode.ExtensionContext) { }
 
-    for (const cell of data.cells) {
-      contents.push({
-        kind: cell.kind,
-        language: cell.languageId,
-        value: cell.value
-      });
-    }
+export function deactivate() { }
