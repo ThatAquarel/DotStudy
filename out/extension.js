149b0221d76def9b88b0a61c502fad5ca4a4da6f
Aquarel <54329158+ThatAquarel@users.noreply.github.com>
2021-10-03 13:10:51 -0400

    Notebook Serializer

--- out/extension.js
+++ out/extension.js
@@ -1,8 +1,36 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deactivate = exports.activate = void 0;
-function activate(context) { }
+const vscode = require("vscode");
+function activate(context) {
+    context.subscriptions.push(vscode.workspace.registerNotebookSerializer('dotstudy-notebook', new SampleSerializer()));
+}
 exports.activate = activate;
+class SampleSerializer {
+    async deserializeNotebook(content, _token) {
+        var contents = new TextDecoder().decode(content);
+        let raw;
+        try {
+            raw = JSON.parse(contents);
+        }
+        catch {
+            raw = [];
+        }
+        const cells = raw.map(item => new vscode.NotebookCellData(item.kind, item.value, item.language));
+        return new vscode.NotebookData(cells);
+    }
+    async serializeNotebook(data, _token) {
+        let contents = [];
+        for (const cell of data.cells) {
+            contents.push({
+                kind: cell.kind,
+                language: cell.languageId,
+                value: cell.value
+            });
+        }
+        return new TextEncoder().encode("test encode");
+    }
+}
 function deactivate() { }
 exports.deactivate = deactivate;
 //# sourceMappingURL=extension.js.map
--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,54 @@
 import * as vscode from 'vscode';
 
-export function activate(context: vscode.ExtensionContext) { }
+export function activate(context: vscode.ExtensionContext) {
+    context.subscriptions.push(
+        vscode.workspace.registerNotebookSerializer('dotstudy-notebook', new SampleSerializer())
+    );
+}
+
+interface RawNotebookCell {
+    language: string;
+    value: string;
+    kind: vscode.NotebookCellKind;
+}
+
+class SampleSerializer implements vscode.NotebookSerializer {
+    async deserializeNotebook(
+        content: Uint8Array,
+        _token: vscode.CancellationToken
+    ): Promise<vscode.NotebookData> {
+        var contents = new TextDecoder().decode(content);
+
+        let raw: RawNotebookCell[];
+        try {
+            raw = <RawNotebookCell[]>JSON.parse(contents);
+        } catch {
+            raw = [];
+        }
+
+        const cells = raw.map(
+            item => new vscode.NotebookCellData(item.kind, item.value, item.language)
+        );
+
+        return new vscode.NotebookData(cells);
+    }
+
+    async serializeNotebook(
+        data: vscode.NotebookData,
+        _token: vscode.CancellationToken
+    ): Promise<Uint8Array> {
+        let contents: RawNotebookCell[] = [];
+
+        for (const cell of data.cells) {
+            contents.push({
+                kind: cell.kind,
+                language: cell.languageId,
+                value: cell.value
+            });
+        }
+
+        return new TextEncoder().encode("test encode");
+    }
+}
 
 export function deactivate() { }
