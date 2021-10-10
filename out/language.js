e5e1e1e0bc250d3d576d42272e51aecfa5244b20
Aquarel <54329158+ThatAquarel@users.noreply.github.com>
2021-10-10 10:26:31 -0400

    Symbol Factory

--- out/language.js
+++ out/language.js
@@ -2,17 +2,23 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.DotStudySymbolProvider = void 0;
 const vscode = require("vscode");
-// function defaultSymbolFactory(line: vscode.TextLine, symbolKind: vscode.SymbolKind): vscode.DocumentSymbol {
-//     return new vscode.DocumentSymbol(
-//         line.text.substring(1), "",
-//         symbolKind,
-//         line.range, line.range
-//     );
-// };
-// const symbolTypes: { [id: string]: (line: vscode.TextLine) => vscode.DocumentSymbol; } = {
-//     "#": (line: vscode.TextLine) => { return defaultSymbolFactory(line, vscode.SymbolKind.Class); },
-//     "!": (line: vscode.TextLine) => { return defaultSymbolFactory(line, vscode.SymbolKind.Method); },
-// };
+function defaultSymbolFactory(line, symbolKind) {
+    return new vscode.DocumentSymbol(line.text.substring(1), "", symbolKind, line.range, line.range);
+}
+;
+function questionSymbolFactory(line) {
+    let indices = [".", "?"].map((separator) => {
+        let i = line.text.indexOf(separator);
+        return (i >= 0) ? i : Infinity;
+    });
+    let end = Math.min(...indices) + 1;
+    return new vscode.DocumentSymbol(line.text.substring(0, end), line.text.substring(end, line.text.length - 1), vscode.SymbolKind.Field, line.range, line.range);
+}
+const symbolTypes = {
+    "#": (line) => { return defaultSymbolFactory(line, vscode.SymbolKind.Class); },
+    "!": (line) => { return defaultSymbolFactory(line, vscode.SymbolKind.Method); },
+    "?": questionSymbolFactory
+};
 class DotStudySymbolProvider {
     provideDocumentSymbols(document, token) {
         return new Promise((resolve, reject) => {
@@ -22,20 +28,17 @@ class DotStudySymbolProvider {
                 let line = document.lineAt(i);
                 let text = line.text;
                 if (text.startsWith("#")) {
-                    parent = new vscode.DocumentSymbol(text.substring(1), "", vscode.SymbolKind.Class, line.range, line.range);
+                    parent = symbolTypes["#"](line);
                 }
                 else if (text.startsWith("!")) {
                     if (symbol) {
                         parent?.children.push(symbol);
                     }
-                    symbol = new vscode.DocumentSymbol(text.substring(1), "test", vscode.SymbolKind.Method, line.range, line.range);
-                }
-                else {
-                    if (text === "") {
-                        continue;
-                    }
                     ;
-                    symbol?.children.push(new vscode.DocumentSymbol(text, "test1 ", vscode.SymbolKind.Field, line.range, line.range));
+                    symbol = symbolTypes["!"](line);
+                }
+                else if (text !== "") {
+                    symbol?.children.push(symbolTypes["?"](line));
                 }
             }
             if (symbol) {
--- src/language.ts
+++ src/language.ts
@@ -1,18 +1,36 @@
 'use strict';
 import * as vscode from 'vscode';
 
-// function defaultSymbolFactory(line: vscode.TextLine, symbolKind: vscode.SymbolKind): vscode.DocumentSymbol {
-//     return new vscode.DocumentSymbol(
-//         line.text.substring(1), "",
-//         symbolKind,
-//         line.range, line.range
-//     );
-// };
+function defaultSymbolFactory(line: vscode.TextLine, symbolKind: vscode.SymbolKind): vscode.DocumentSymbol {
+    return new vscode.DocumentSymbol(
+        line.text.substring(1), "",
+        symbolKind,
+        line.range, line.range
+    );
+};
 
-// const symbolTypes: { [id: string]: (line: vscode.TextLine) => vscode.DocumentSymbol; } = {
-//     "#": (line: vscode.TextLine) => { return defaultSymbolFactory(line, vscode.SymbolKind.Class); },
-//     "!": (line: vscode.TextLine) => { return defaultSymbolFactory(line, vscode.SymbolKind.Method); },
-// };
+function questionSymbolFactory(line: vscode.TextLine): vscode.DocumentSymbol {
+    let indices = [".", "?"].map((separator) => {
+        let i = line.text.indexOf(separator);
+
+        return (i >= 0) ? i : Infinity;
+    });
+
+    let end = Math.min(...indices) + 1;
+
+    return new vscode.DocumentSymbol(
+        line.text.substring(0, end),
+        line.text.substring(end, line.text.length - 1),
+        vscode.SymbolKind.Field,
+        line.range, line.range
+    );
+}
+
+const symbolTypes: { [id: string]: (line: vscode.TextLine) => vscode.DocumentSymbol; } = {
+    "#": (line: vscode.TextLine) => { return defaultSymbolFactory(line, vscode.SymbolKind.Class); },
+    "!": (line: vscode.TextLine) => { return defaultSymbolFactory(line, vscode.SymbolKind.Method); },
+    "?": questionSymbolFactory
+};
 
 export class DotStudySymbolProvider implements vscode.DocumentSymbolProvider {
 
@@ -28,29 +46,12 @@ export class DotStudySymbolProvider implements vscode.DocumentSymbolProvider {
                 let text = line.text;
 
                 if (text.startsWith("#")) {
-                    parent = new vscode.DocumentSymbol(
-                        text.substring(1), "",
-                        vscode.SymbolKind.Class,
-                        line.range, line.range
-                    );
+                    parent = symbolTypes["#"](line);
                 } else if (text.startsWith("!")) {
-                    if (symbol) {
-                        parent?.children.push(symbol);
-                    }
-                    symbol = new vscode.DocumentSymbol(
-                        text.substring(1),
-                        "test",
-                        vscode.SymbolKind.Method,
-                        line.range, line.range
-                    );
-                } else {
-                    if (text === "") { continue; };
-                    symbol?.children.push(new vscode.DocumentSymbol(
-                        text,
-                        "test1 ",
-                        vscode.SymbolKind.Field,
-                        line.range, line.range
-                    ));
+                    if (symbol) { parent?.children.push(symbol); };
+                    symbol = symbolTypes["!"](line);
+                } else if (text !== "") {
+                    symbol?.children.push(symbolTypes["?"](line));
                 }
             }
 
