97d50b9b86104a79c9dfbb890a81efc42cf0e4d2
Aquarel <54329158+ThatAquarel@users.noreply.github.com>
2021-10-10 10:03:17 -0400

    Symbol Provider

--- out/extension.js
+++ out/extension.js
@@ -1,36 +1,14 @@
-"use strict";
+'use strict';
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deactivate = exports.activate = void 0;
 const vscode = require("vscode");
+const preview_1 = require("./preview");
+const language_1 = require("./language");
 function activate(context) {
-    context.subscriptions.push(vscode.workspace.registerNotebookSerializer('dotstudy-notebook', new SampleSerializer()));
+    context.subscriptions.push(preview_1.DotStudyEditorProvider.register(context));
+    context.subscriptions.push(vscode.languages.registerDocumentSymbolProvider({ language: "dotstudy-language" }, new language_1.DotStudySymbolProvider()));
 }
 exports.activate = activate;
-class SampleSerializer {
-    async deserializeNotebook(content, _token) {
-        var contents = new TextDecoder().decode(content);
-        let raw;
-        try {
-            raw = JSON.parse(contents);
-        }
-        catch {
-            raw = [];
-        }
-        const cells = raw.map(item => new vscode.NotebookCellData(item.kind, item.value, item.language));
-        return new vscode.NotebookData(cells);
-    }
-    async serializeNotebook(data, _token) {
-        let contents = [];
-        for (const cell of data.cells) {
-            contents.push({
-                kind: cell.kind,
-                language: cell.languageId,
-                value: cell.value
-            });
-        }
-        return new TextEncoder().encode("test encode");
-    }
-}
 function deactivate() { }
 exports.deactivate = deactivate;
 //# sourceMappingURL=extension.js.map
