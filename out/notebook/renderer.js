0b8e3a9a592ec14075730c16f163d0246cd26a2d
Aquarel <54329158+ThatAquarel@users.noreply.github.com>
2021-10-11 09:41:53 -0400

    Discord Plublish

--- out/notebook/renderer.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// import type { ActivationFunction } from 'vscode-notebook-renderer';
-// export const activate: ActivationFunction = context => ({
-//     renderOutputItem(data, element) {
-//         element.innerText = "dotstudy notebook renderer";
-//     }
-// });
-//# sourceMappingURL=renderer.js.map
--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,17 @@
 'use strict';
 import * as vscode from 'vscode';
+import { commands } from 'vscode';
+import { DotStudySymbolProvider } from './language';
 import { DotStudyEditorProvider } from './preview';
-import {DotStudySymbolProvider} from './language';
+import { publish } from './publish';
 
-export function activate(context: vscode.ExtensionContext) { 
+export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(DotStudyEditorProvider.register(context));
     context.subscriptions.push(vscode.languages.registerDocumentSymbolProvider(
-        {language: "dotstudy-language"}, new DotStudySymbolProvider()
+        { language: "dotstudy-language" }, new DotStudySymbolProvider()
     ));
+
+    context.subscriptions.push(commands.registerCommand('dotstudy.publish', publish));
 }
 
 export function deactivate() { }
