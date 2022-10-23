94dae1ec0e92d22d0e3164b0ab111850ea3acfa4
Aquarel <54329158+ThatAquarel@users.noreply.github.com>
2022-10-23 16:56:48 -0400

    remove all legacy features

--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,6 @@
 'use strict';
 import * as vscode from 'vscode';
-import { commands } from 'vscode';
-import { DotStudySymbolProvider } from './language';
-import { DotStudyEditorProvider } from './preview';
-import { publish } from './publish';
 
-export function activate(context: vscode.ExtensionContext) {
-    context.subscriptions.push(DotStudyEditorProvider.register(context));
-    context.subscriptions.push(vscode.languages.registerDocumentSymbolProvider(
-        { language: "dotstudy-language" }, new DotStudySymbolProvider()
-    ));
-
-    context.subscriptions.push(commands.registerCommand('dotstudy.publish', publish));
-}
+export function activate(_context: vscode.ExtensionContext) {}
 
 export function deactivate() { }
